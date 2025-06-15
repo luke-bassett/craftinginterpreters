1725a4655e7161b2b4190ae0c4d1b8ff257abca4
Luke Bassett <bassett.luke@gmail.com>
2025-06-15 10:05:40 -0700

    google-java-format

--- jlox/com/craftinginterpreters/lox/Lox.java
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -9,69 +9,68 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-    static boolean hadError = false;
+  static boolean hadError = false;
 
-    public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
-            System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
-        }
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
     }
+  }
 
-    private static void runFile(String path) throws IOException {
-        // read the file at path and store it as an array of bytes
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
-        // explicity choose the default charset, I think this is the same as omitting it
-        run(new String(bytes, Charset.defaultCharset()));
+  private static void runFile(String path) throws IOException {
+    // read the file at path and store it as an array of bytes
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    // explicity choose the default charset, I think this is the same as omitting it
+    run(new String(bytes, Charset.defaultCharset()));
 
-        if (hadError) System.exit(65);
+    if (hadError) System.exit(65);
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    // `for (;;)` will run until exited I think
+    for (; ; ) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+      hadError = false;
     }
+  }
 
-    private static void runPrompt() throws IOException {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
 
-        // `for (;;)` will run until exited I think
-        for (;;) {
-            System.out.print("> ");
-            String line = reader.readLine();
-            if (line == null) break;
-            run(line);
-            hadError = false;
-        }
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
     }
+  }
 
-    private static void run(String source) {
-        Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.scanTokens();
-
-        Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
-
-        if (hadError) return;
-
-        System.out.println(new AstPrinter().print(expression));
-    }
-
-    static void error(int line, String message) {
-        report(line, "", message);
-    }
-
-    static void error(Token token, String message) {
-        if (token.type == TokenType.EOF) {
-            report(token.line, " at end", message);
-        } else {
-            report(token.line, " at '" + token.lexeme +"'", message);
-        }
-    }
-
-    private static void report(int line, String where, String message) {
-        System.err.println(
-            "[line " + line + "] Error" + where + ": " + message);
-        hadError = true;
-    }
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
 }
--- jlox/com/craftinginterpreters/lox/Scanner.java
+++ jlox/com/craftinginterpreters/lox/Scanner.java
@@ -6,202 +6,195 @@ import java.util.List;
 import java.util.Map;
 
 class Scanner {
-    private final String source;
-    private final List<Token> tokens = new ArrayList<>();
+  private final String source;
+  private final List<Token> tokens = new ArrayList<>();
 
-    private static final Map<String, TokenType> keywords;
-    static {
-        keywords = new HashMap<>();
-        keywords.put("and", TokenType.AND);
-        keywords.put("class", TokenType.CLASS);
-        keywords.put("else", TokenType.ELSE);
-        keywords.put("false", TokenType.FALSE);
-        keywords.put("for", TokenType.FOR);
-        keywords.put("fun", TokenType.FUN);
-        keywords.put("if", TokenType.IF);
-        keywords.put("nil", TokenType.NIL);
-        keywords.put("or", TokenType.OR);
-        keywords.put("print", TokenType.PRINT);
-        keywords.put("return", TokenType.RETURN);
-        keywords.put("super", TokenType.SUPER);
-        keywords.put("this", TokenType.THIS);
-        keywords.put("true", TokenType.TRUE);
-        keywords.put("var", TokenType.VAR);
-        keywords.put("while", TokenType.WHILE);
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", TokenType.AND);
+    keywords.put("class", TokenType.CLASS);
+    keywords.put("else", TokenType.ELSE);
+    keywords.put("false", TokenType.FALSE);
+    keywords.put("for", TokenType.FOR);
+    keywords.put("fun", TokenType.FUN);
+    keywords.put("if", TokenType.IF);
+    keywords.put("nil", TokenType.NIL);
+    keywords.put("or", TokenType.OR);
+    keywords.put("print", TokenType.PRINT);
+    keywords.put("return", TokenType.RETURN);
+    keywords.put("super", TokenType.SUPER);
+    keywords.put("this", TokenType.THIS);
+    keywords.put("true", TokenType.TRUE);
+    keywords.put("var", TokenType.VAR);
+    keywords.put("while", TokenType.WHILE);
+  }
+
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
+
+  Scanner(String source) {
+    this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      start = current;
+      scanToken();
     }
 
+    tokens.add(new Token(TokenType.EOF, "", null, line));
+    return tokens;
+  }
 
-    private int start = 0;
-    private int current = 0;
-    private int line = 1;
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      // single character lexemes
+      case '(' -> addToken(TokenType.LEFT_PAREN);
+      case ')' -> addToken(TokenType.RIGHT_PAREN);
+      case '{' -> addToken(TokenType.LEFT_BRACE);
+      case '}' -> addToken(TokenType.RIGHT_BRACE);
+      case ',' -> addToken(TokenType.COMMA);
+      case '.' -> addToken(TokenType.DOT);
+      case '-' -> addToken(TokenType.MINUS);
+      case '+' -> addToken(TokenType.PLUS);
+      case ';' -> addToken(TokenType.SEMICOLON);
+      case '*' -> addToken(TokenType.STAR);
 
-    Scanner(String source) {
-        this.source = source;
-    }
-
-    List<Token> scanTokens() {
-        while (!isAtEnd()) {
-            start = current;
-            scanToken();
+      // 1 or 2 character lexemes
+      case '!' -> addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+      case '=' -> addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+      case '<' -> addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+      case '>' -> addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.EQUAL);
+      case '/' -> {
+        if (match('/')) {
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(TokenType.SLASH);
         }
+      }
 
-        tokens.add(new Token(TokenType.EOF, "", null, line));
-        return tokens;
-    }
+      // whitespace
+      case ' ', '\r', '\t' -> {}
+      case '\n' -> line++;
 
-    private void scanToken() {
-        char c = advance();
-        switch (c) {
-            // single character lexemes
-            case '(' -> addToken(TokenType.LEFT_PAREN);
-            case ')' -> addToken(TokenType.RIGHT_PAREN);
-            case '{' -> addToken(TokenType.LEFT_BRACE);
-            case '}' -> addToken(TokenType.RIGHT_BRACE);
-            case ',' -> addToken(TokenType.COMMA);
-            case '.' -> addToken(TokenType.DOT);
-            case '-' -> addToken(TokenType.MINUS);
-            case '+' -> addToken(TokenType.PLUS);
-            case ';' -> addToken(TokenType.SEMICOLON);
-            case '*' -> addToken(TokenType.STAR);
+      // strings
+      case '"' -> string();
 
-            // 1 or 2 character lexemes
-            case '!' ->
-                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
-            case '=' ->
-                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
-            case '<' ->
-                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
-            case '>' ->
-                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.EQUAL);
-            case '/' -> {
-                if (match('/')) {
-                    while (peek() != '\n' && !isAtEnd()) advance();
-                } else {
-                    addToken(TokenType.SLASH);
-                }
-            }
-
-            // whitespace
-            case ' ', '\r', '\t' -> { }
-            case '\n' -> line++;
-
-            // strings
-            case '"' -> string();
-
-            default -> {
-                if (isDigit(c)) {
-                    number();
-                } else if (isAlpha(c)) {
-                    identifier();
-                } else {
-                    Lox.error(line, "Unexpected character '" + c + "'");
-                }
-            }
+      default -> {
+        if (isDigit(c)) {
+          number();
+        } else if (isAlpha(c)) {
+          identifier();
+        } else {
+          Lox.error(line, "Unexpected character '" + c + "'");
         }
+      }
+    }
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') {
+        line++;
+      }
+      advance();
+
+      if (isAtEnd()) {
+        Lox.error(line, "Unterminated string.");
+        return;
+      }
+
+      // only get here with the closing "
+      advance();
+
+      String value = source.substring(start + 1, current - 1);
+      addToken(TokenType.STRING, value);
+    }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) {
+      type = TokenType.IDENTIFIER;
+    }
+    addToken(type);
+  }
+
+  private void number() {
+    // advance as long as we keep seeing digits
+    while (isDigit(peek())) advance();
+
+    // look for an optional fractional part
+    if (peek() == '.' && isDigit(peekNext())) {
+      advance();
+      while (isDigit(peek())) advance();
     }
 
-    private void string() {
-        while (peek() != '"' && !isAtEnd()) {
-            if (peek() == '\n') {
-                line++;
-            }
-            advance();
+    String value = source.substring(start, current);
+    addToken(TokenType.NUMBER, value);
+  }
 
-        if (isAtEnd()) {
-            Lox.error(line, "Unterminated string.");
-            return;
-        }
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
 
-        // only get here with the closing "
-        advance();
+  private char advance() {
+    return source.charAt(current++);
+  }
 
-        String value = source.substring(start +1, current - 1);
-        addToken(TokenType.STRING, value);
-        }
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
+
+  // match is like a conditional advance, it only consume the current
+  // character if it's the one we were looking for
+  private boolean match(char expected) {
+    if (isAtEnd()) {
+      return false;
     }
-
-    private void identifier() {
-        while (isAlphaNumeric(peek())) advance();
-
-        String text = source.substring(start, current);
-        TokenType type = keywords.get(text);
-        if (type == null) {
-            type = TokenType.IDENTIFIER;
-        }
-        addToken(type);
+    if (source.charAt(current) != expected) {
+      return false;
     }
+    current++;
+    return true;
+  }
 
-    private void number() {
-        // advance as long as we keep seeing digits
-        while (isDigit(peek())) advance();
-
-        // look for an optional fractional part
-        if (peek() == '.' && isDigit(peekNext())) {
-            advance();
-            while (isDigit(peek())) advance();
-        }
-
-        String value = source.substring(start, current);
-        addToken(TokenType.NUMBER, value);
+  private char peek() {
+    if (isAtEnd()) {
+      return '\0';
     }
+    return source.charAt(current);
+  }
 
-    private boolean isAtEnd() {
-        return current >= source.length();
+  private char peekNext() {
+    int next = current + 1;
+    if (next >= source.length()) {
+      return '\0';
     }
+    return source.charAt(next);
+  }
 
-    private char advance() {
-        return source.charAt(current++);
-    }
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+  }
 
-    private void addToken(TokenType type) {
-        addToken(type, null);
-    }
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
 
-    private void addToken(TokenType type, Object literal) {
-        String text = source.substring(start, current);
-        tokens.add(new Token(type, text, literal, line));
-    }
-
-    // match is like a conditional advance, it only consume the current
-    // character if it's the one we were looking for
-    private boolean match(char expected) {
-        if (isAtEnd()) {
-            return false;
-        }
-        if (source.charAt(current) != expected) {
-            return false;
-        }
-        current++;
-        return true;
-    }
-
-    private char peek() {
-        if (isAtEnd()) {
-            return '\0';
-        }
-        return source.charAt(current);
-    }
-
-    private char peekNext() {
-        int next = current + 1;
-        if (next >= source.length()) {
-            return '\0';
-        }
-        return source.charAt(next);
-    }
-
-    private boolean isAlpha(char c) {
-        return (c >= 'a' && c <= 'z') ||
-               (c >= 'A' && c <= 'Z') ||
-                c == '_';
-    }
-
-    private boolean isDigit(char c) {
-        return c >= '0' && c <= '9';
-    }
-
-    private boolean isAlphaNumeric(char c) {
-        return isAlpha(c) || isDigit(c);
-    }
-
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
 }
